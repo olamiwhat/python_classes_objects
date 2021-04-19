2869e6433eeac5649f2c5dab23b92d2c488f1047
Olamilekan Adedoyin <olalinux03@gmail.com>
2021-04-18 22:17:14 -0400

    add restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,19 @@
+'''
+Restaurant: Make a class called Restaurant.
+
+The __init__() method for Restaurant should store two attributes:
+ - a restaurant_name
+ - and a cuisine_type.
+
+Make a method called describe_restaurant() that prints these two pieces of
+information, and a method called open_restaurant() that prints a message indi-
+cating that the restaurant is open.
+
+Make an instance called restaurant from your class. Print the two attri-
+butes individually, and then call both methods.
+
+
+Three Restaurants: Start with your class from above. Create three
+different instances from the class, and call describe_restaurant() for each
+instance.
+'''
